50a0ffc6cb4f7f833dd4462b565c8590ba05be8a
Aidan McCarthy <aidan@Aidans-MacBook-Pro.local>
2024-07-18 01:29:50 -0400

    Last script setup before implementing ocr usage

--- tool.py
+++ tool.py
@@ -1,14 +1,18 @@
 import sys
+import platform
+import pytesseract
+from pdf2image import convert_from_path
+from PIL import Image
+from tempfile import TemporaryDirectory
+from pathlib import Path
+import openai
+
+openai.api_key = '<API_KEY>'
+outdir = Path(__file__).parent.resolve() # Directory of script
 
 def main(pdfName):
     print(pdfName)
-
-
-
-
-
-
-
+    print(outdir)
 
 if __name__ == "__main__":
     main(sys.argv[1])
