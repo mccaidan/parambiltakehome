dd0b9addc5ac035ceb566d9b6fa4109e33fdce62
Aidan McCarthy <aidan@Aidans-MacBook-Pro.local>
2024-07-19 01:12:54 -0400

    Used API for surgeries

--- api.py
+++ api.py
@@ -15,95 +15,313 @@ def comp(PROMPT=''):
     print(respDict)
     
 
-prompt = """which allergies are listed here: Page 2 of 2
+prompt = """which surgeries are listed here, with their date, using None for unknown: Phone: (650)723-6601 Fax: (650)725-8910
 
--end002421772
+Patient Name: CHASE, JEANINE Subject Code: 09148JC5 Study Date: 09-14-08
 
-Stanford Hospital and Clinics
-300 Pasteur Drive
+Oxygen Saturation
+
+PARAMETER
+
+Mean SaQ2 %
+Min SaO2 %
+Max Sa02 %
+
+% Duration of SaO2 In
+
+Range :
+90 - 100 %
+80 - 90 %
+70 - 80%
+60-70%
+50 - 60 %
+Below 50 %
+
+PLMs and Sleep Stages
+
+INDEX INDEX TOTAL
+0.0
+0.0
+
+Total Sleep
+Stage |
+Stage 2
+Stage 3
+Stage 4
+
+Stage REM
+
+PLM Events With Arousals PLM Events W/O Arousals
+
+PARAMETER INDEX PARAMETER | INDEX TOTAL
+
+Total Events 0.0 Total Events 0.0
+0.0
+0.0
+
+NREM 0.0
+REM 0.0
+
+15-1664 (Jan-00) 3
+Stanford Sleep Disorders Clinic
+401 Quarry Road
 Stanford, CA 94305
 
-NAME: Chase, Jeanine NEW PATIENT VISIT
+Phone: (650)723-6601 Fax: (650)725-8910
 
-NUMBER: 177-57-97 ACCOUNT NUMBER: 060017757972
-ATTENDING: Michael Fredericson, M.D.
+Patient Name: CHASE, JEANINE Subject Code: 09148JC5 Study Date: 09-14-08
 
-DATE: 06/17/2004
+Pressure Level Analysis
 
-****%* ALLERGIES*****THE PATIENT IS ALLERGIC TO PENICILLIN, THIS CAUSES
-SWELLING OF HER AIRWAY AND SHE CANNOT DIGEST WHEAT OR SOY.
+Pressure TRT REM Non- | Obstr. | Cent. | Mixed | Hypo- | Total Sa02 SaQ2 SaOQ2
+(cm H20) (min) | (min) REM | Apnea | Apnea | Apnea } pneas | Events High Low Mean
+(min
 
-IDENTIFICATION: The patient is a 31-year-old female with complaints of right groin pain.
+Recording Technician’s (MR) Comments :
 
-HISTORY OF PRESENT ILLNESS: Ms. Chase comes to clinic today complaining of right groin
-pain and tightness. The patient states that she injured her gluteus medius during a yoga position
-where she tried to put her right foot behind her head. She feels she also strained her back a little at
-the same time. She was evaluated by Dr. Saal on February 15, 2004. He diagnosed her as having
-chronic gluteus medius tendinitis as suggested by calcifications adjacent to the greater trochanter on
-x-ray. He also did a lumbar spine x-ray, which revealed normal findings. The patient was referred
-to a Pilates class for core strengthening and flexibility exercises and she was also referred to the
-Sports Medicine Institute for deep tissue massage. She has been going to SMI working with Rob
-Finney over the last two years. The patient also notes that she switched for a short time to the
-Sports Massage Center and was treated with a Stem machine but felt like her injury was getting
-worse so she returned to the Sports Medicine Institute. Today she states her pain level is 0/10. She
-states that she would like to return to cycling but notes that activities such as cycling cause a
-rubbing sensation in the nght groin area as well as she feels the need for popping her hip frequently.
-The patient notes worsening of her symptoms with cycling, prolonged electrical use, and any hip
-adductor or hip abductor muscle exercises.
+Scoring Technician’s (JA) Comments :
 
-PAST MEDICAL HISTORY: As noted above.
-PAST SURGICAL HISTORY: None.
-MEDICATIONS: None.
+Patient slept mostly in stage 2.
 
-FAMILY HISTORY: The patient’s father is 73 and has stomach problems. Her mother is 63 and
-has a bad knee. She has one sibling who is 30 and has stress problems. She had another sibling
-who was 30 and had drug and Vicodin problems, who is deceased secondary to suicide. The
-patient’s grandparents both have diabetes and are ages 98 and 85.
+Flow limitations, hypopneas, and apneas noted, often cyclical.
+Snoring noted.
 
-SOCIAL HISTORY: The patient denies tobacco use or illicit drug use. She states that she drinks
+15-1664 (Jan-00) 4
+LEGS
 
-alcohol once a month. She eats a balanced diet and her weight is stable. She was born in California
-and currently works as a hair stylist. She lives alone.
+BODY POS
 
-nim
-
-Page 1 of 3Original for Scanning
-
-002421772
-
-Stanford Hospital and Clinics
-300 Pasteur Drive
+Stanford Sleep Disorders Clinic
+401 Quarry Road
 Stanford, CA 94305
 
-NAME: Chase, Jeanine NEW PATIENT VISIT
+Phone: (650)723-6601 Fax: (650)725-8910
 
-NUMBER: 177-57-97 ACCOUNT NUMBER: 060017757972
-ATTENDING: Michael Fredericson, M.D.
+Patient Name: CHASE, JEANINE Subject Code: 09148JC5 Study Date: 09-14-08
+GRAPHS
 
-DATE: 06/17/2004
+STAGING
+n
+ry
+ao
+&
 
-REVIEW OF SYSTEMS: The patient is in good general health lately. She notes some recent
-weight changes secondary to dietary changes. She otherwise denies other constitutional symptoms.
-She denies eyes, ears, nose, mouth, or throat disorders. She denies respiratory, cardiovascular, or
-gastrointestinal problems. She denies neurologic, psychiatric, endocrine, hematologic, or lymphatic
-disorders. The patient notes some burning with urination but denies any other genitourinary
-disorders. The patient notes some changing moles but no other integumentary disorders. She states
-she has joint stiffness, pain and cramping, weakness of muscles and joints, and some back pain.
-The patient also notes allergic reaction to medications and that she has had her tetanus booster
-within the last ten years. Her other immunizations are up to date.
+22:00 23:00 00:00 01:00 02:00 03:00 04:00 05:00 06:00 07:00
 
-PHYSICAL EXAMINATION: On physical examination the patient is a pleasant woman in no
-acute distress. She has genu varus alignment of her lower extremities and pes planus bilaterally
-with the too many toes sign bilaterally. She has no pain with single leg squat and no pain with
-single leg hop. She is non-tender with resisted hip flexion, hip adduction, or hip abduction. She has
-a negative fabere test. She has tenderness of the right paraspinal muscles at the level of L5. She has
-pain with internal rotation, hip flexion, and adduction of the right groin. She has a positive Sculler
-test. The patient is slightly guarded at the right hip with internal rotation. She has good range of
-motion of both hips bilaterally. She has a little snapping in the groin with internal rotation and hip
-flexion. The patient is non-tender at the SI joint and sacrurn. The patient has excessive pronation
-with ambulation. She has weakness of the hip flexors and hip abductors and a negative Thomas
-test. She is non-tender at the greater trochanter on the right. She has a negative femoral cutaneous
-stretch test. She has no pain with lumbar forward flexion or hyperextension. She has some
-tightness with side bending to the right with rotation."""
+ra Imogliwl w TT Oe ieee ot COG i] \a mm) @ Bawa) ii wi alt
+Oo
+e wsmel tie) sae) 6§l lc ae uD) oom ton a a tI SS) ie Ll nr
+wl
+22:00 23:00 00:00 01:00 02:00 03:00 04:00 05:00 06:00 07:00
+Both Legs
+Righ
+Left
+Both Legs+Ar
+Righf +Arsl
+Left + Arsl
+22:00 23:00 00:00 01:00 02:00 03:00 04:00 05:00 06:00 07:00
+>
+or
+|
+Lu
+2
+iS
+22:00 23:00 00:00 01:00 02:00 03:00 04:00 05:00 06:00 07:00
+Supine
+Prone
+Left
+ri
+Out of Bed
+22:00 23:00 00:00 01:00 02:00 03:00 04:00 05:00 06:00 07:00
+20
+a
+< 10
+Oo
+0
+22:00 23:00 00:00 01:00 02:00 03:00 04:00 05:00 06:00 07:00
+20
+Tr
+iu
+=
+o 0
+22:00 23:00 00:00 01:00 02:00 03:00 04:00 05:00 06:00 07:00
+
+15-1664 (Jan-00) )
+-end
+Wyatt Jaffe
+
+Mark Twain Medical Center
+May 15th, 2023 1:39pm
+May 15th, 2023 1:39pm
+
+Mark Twain Medical Center
+768 Mountain Ranch Rd.
+San Andreas, CA 95249
+
+ED H&P
+
+Patient Sone MR Number: CHOO
+154502
+
+Age: 51 DOB: 08/19/1971 Sex: F
+
+Acct Number: MT4401207131
+
+Attending Physician: Admit Date: 05/15/23
+KMR Number: Admit Time: 1304
+
+Report Status: Signed
+
+History of Present Illness
+
+Time Seen by Provider: 05/15/23 13:35
+Chief Complaint: Ear Complaints
+
+HPI:
+
+Patient is a 51-year-old female who comes in complaining of increasing pain in the right ear for the last 4 days. Patient been trying saline nasal sprays as well as Mucinex without much relief. She has a history of multiple surgeries onher sinuses and jaw for due to sleep apnea. Patient also said over the last week she had a URI and saw her primary who gave her prescription for antibiotics. She says she finished that few days ago. He said that since then she still felt a lot of congestion and now pain in her right ear. No discharge from the ear. No fever. No other significant complaints.
+Past Medical Surgical History:
+
+Past Medical-Surgical History Start: 05/15/23 13:05
+
+Freq: Status: Active
+
+Protocol:
+
+Document 05/15/23 13:18 KREIEM (Rec: 05/15/23 13:25 KREIEM EDCO2MEDVDA1002)
+Past Medical-Surgical History
+
+Do You Have Any Past Medical or Surgical Yes
+
+History [ACEDMPM]
+
+EENT Medical-Surgical Details/Date MAXILLARY-MANDIBULAR JAW
+ADVANCEMENT SURGERY @STANFORD
+
+2012
+
+Respiratory Medical History Sleep apnea
+
+Has Patient Had A Prior Colonoscopy? No.
+
+Has Patient Had a Prior Endoscopy? No
+
+Reproductive surgical history Hysterectomy
+
+LMP 3/11/2018
+
+Pregnant No
+
+Musculoskeletal surgical history Hip Surgery
+
+Patient with History of Venous No.
+
+Thromboembolism (VTE)?
+
+Psychosocial medical disorder Anxiety, Depression
+
+Social History:
+
+Alcohol Frequency Occasionally
+Alcohol Use Ever Caused a No
+Problem for You or Your Family
+Alcohol Use Current
+Recreational Drug Use None
+
+Any previous attempts of self No
+harm/suicide
+
+Primary Dx or stated complaint No
+of emotional/beh disorder
+
+Source of history: Patient
+
+Allergies and Home Meds
+Home Medications:
+
+Medication Instructions Recorded
+
+QUEtiapine [Seroquel] 1 tab PO BEDTIME #30 tab 07/02/19
+zolpidem [Ambien] 5 mg PO BEDTIME PRN 07/03/19
+atorvastatin [Lipitor] 10 mg PO BEDTIME 03/31/21
+gabapentin [Gabapentin] 300 mg PO TID 04/20/22
+Citalopram Hydrobromide 40 mg PO QAM 03/23/23
+[Citalopram HBr]
+
+methylphenidate HC! 30 mg PO QAM 03/23/23
+[Methylphenidate HCI ER]
+
+Guaifenesin/Pseudoephedrne HCI 1 each PO BID #14 tab.er.12h 05/15/23
+[Mucinex D ER 1,200-120 mg Tab]
+
+Oxymetazoline HCI [Afrin] 1 spray NASAL BID #15 ml 05/15/23
+
+Allergies/Adverse Reactions:
+
+Allergy/AdvReac Type Severity Reaction Status Date / Time
+
+Penicillins Allergy Severe Anaphylaxis Verified 03/23/23 09:57
+
+Reviewed the patient's home medications and allergies:: Yes
+
+ED Review of Systems
+Statement: All ROS negative unless mentioned in HPI
+
+Physical Exam
+Vital Signs:
+
+Temp Pulse Resp BP Pulse Ox
+05/15/23 13:18 99.0 F 100 16 137/85 97
+
+Height: 5 ft 2 in
+Weight: 86.636 kg
+BMI: 34.9
+
+Pulse Ox: normal
+
+General Appearence: no acute distress, alert, oriented X3, well nourished, well developed, well appearing
+
+Neuro: normal mood, normal affect, normal speech, normal gait, moves all 4 extremities. No: motor weakness, sensory deficit
+HEENT: ENT appears normal, normocephalic atraumatic, EOMs intact, PEERL, tympanic membranes* (Clear bilaterally no erythema)
+Neck: supple, non-tender, atraumatic, painless ROM
+
+Respiratory: lungs clear, normal air movement, speaks full sentences
+
+Cardiovascular: regular rate & rhythm, normal peripheral pulses, no murmur
+
+MDM & Depart
+
+Differential Diagnosis:
+
+Multiple differential diagnoses were considered in this patient
+Reviewed by me today: Medications
+
+Was an EKG Done?: No
+
+ED Course:
+
+Based on history and physical my impression is the patient likely has eustachian tube dysfunction. Recommend more aggressive decongestants with Afrin and pseudoephedrine. These prescriptions were given. Indications given to return. Patient verbalized understanding and agreement with plan and discharge.
+Condition: Stable
+
+Plan of Care discussed with: Patient
+
+Clinical Impression/Final Diagnosis:
+
+Eustachian tube dysfunction
+
+Qualifiers:
+
+Laterality: right Qualified Code(s): H69.81 - Other specified disorders of Eustachian tube, right ear
+
+ED Disposition: Discharged
+ED H&P Completed: Yes
+
+Report Entered By: Jaffe, Wyatt MD
+Electronically Signed: Jaffe, Wyatt MD
+
+Sign Date/Time: 05/15/23 1339
+<Electronically signed by Wyatt Jaffe, MD>
+Entered D/T: 05/15/23 1337 By:JAFFWY
+Report #: 0515-0033"""
 
 comp(prompt)
