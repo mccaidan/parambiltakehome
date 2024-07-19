f20fc0ac60d70a505e25c29ee2585cf5c17bd9b7
Aidan McCarthy <aidan@Aidans-MacBook-Pro.local>
2024-07-19 01:02:25 -0400

    Used API for medications

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ from openai import OpenAI
 client = OpenAI(api_key='')
 
 def comp(PROMPT=''):
-    print('Function called')
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
         response_format={ "type": "json_object" },
